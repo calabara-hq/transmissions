2de46ddd83b4346c1914c54efe4cda953ee33150
ndodson <nick@instance-20240315-161943.us-east1-b.c.remote-dev-417316.internal>
2024-03-26 15:41:51 +0000

    progress

--- src/channel/ChannelImpl.sol
+++ src/channel/ChannelImpl.sol
@@ -5,37 +5,37 @@ import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.so
 import {IChannel} from "../interfaces/IChannel.sol";
 import {IChannelTypesV1} from "../interfaces/IChannelTypesV1.sol";
 import {ChannelStorageV1} from "../storage/ChannelStorageV1.sol";
-import {ChannelFeeStorageV1} from "../storage/ChannelFeeStorageV1.sol";
 import {Escrow} from "../escrow/EscrowImpl.sol";
 import {Multicall} from "../utils/Multicall.sol";
-import {PaidSaleStrategy} from "../sales/PaidSalesStrategy.sol";
+import {CustomSaleStrategy} from "../sales/CustomSaleStrategy.sol";
 import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
 import {SharedBaseConstants} from "../shared/SharedBaseConstants.sol";
+import {ISales} from "../sales/ISales.sol";
 
 contract Channel is
     IChannel,
     IChannelTypesV1,
     ChannelStorageV1,
-    ChannelFeeStorageV1,
     Escrow,
     Multicall,
-    PaidSaleStrategy,
+    CustomSaleStrategy,
     Initializable,
     UUPSUpgradeable,
     SharedBaseConstants
 {
-    //FeePair[] public channelFees;
-
     constructor(address _platformRecipient) initializer {
-        // nextTokenId = 1;
+        uplinkFeeAddress = _platformRecipient;
     }
 
     /// @notice initializer
     /// @param uri the uri for the token
     /// @param setupActions the setup actions for the channel
 
-    function initialize(string calldata uri, bytes[] calldata setupActions) external initializer {
+    function initialize(
+        string calldata uri,
+        bytes[] calldata setupActions
+    ) external initializer {
         __UUPSUpgradeable_init();
 
         if (setupActions.length > 0) {
@@ -55,28 +55,28 @@ contract Channel is
     //     return nextTokenId++;
     // }
 
-    // function createToken(string calldata uri, address author, uint256 maxSupply, address createReferral)
-    //     public
-    //     returns (uint256)
-    // {
-    //     // todo: implement incrementing tokenId
-    //     uint256 tokenId = getNextTokenId();
+    function createToken(
+        string calldata uri,
+        address author,
+        uint256 maxSupply,
+        ISales saleContract,
+        bytes calldata saleArgs
+    ) public returns (uint256) {
+        // todo: implement incrementing tokenId
+        uint256 tokenId = 1; //getNextTokenId();
 
-    //     // need to store the create referral address somewhere (maybe not here in the token config)
-    //     // do we need to store the author address somewhere?
+        TokenConfig memory tokenConfig = TokenConfig({
+            uri: uri,
+            maxSupply: maxSupply,
+            totalMinted: 0
+        });
 
-    //     TokenConfig memory tokenConfig = TokenConfig({
-    //         uri: uri,
-    //         maxSupply: maxSupply,
-    //         totalMinted: 0,
-    //         author: author,
-    //         createReferral: createReferral
-    //     });
+        tokens[tokenId] = tokenConfig;
 
-    //     tokens[tokenId] = tokenConfig;
+        saleContract.handleCreateSale(tokenId, saleArgs);
 
-    //     return tokenId;
-    // }
+        return tokenId;
+    }
 
     // function mint(address minter, uint256 tokenId, uint256 amount, address mintReferral) external payable {
     //     // todo: checks for minting rules
@@ -120,7 +120,9 @@ contract Channel is
     //     return price;
     // }
 
-    function getTokens(uint256 tokenId) public view returns (TokenConfig memory) {
+    function getTokens(
+        uint256 tokenId
+    ) public view returns (TokenConfig memory) {
         return tokens[tokenId];
     }
 
--- test/channel/Channel.t.sol
+++ test/channel/Channel.t.sol
@@ -5,61 +5,48 @@ import {Test, console} from "forge-std/Test.sol";
 import {ChannelFactory} from "../../src/factory/ChannelFactoryImpl.sol";
 import {Channel} from "../../src/channel/ChannelImpl.sol";
 import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
-import {ChannelFeeStorageV1} from "../../src/storage/ChannelFeeStorageV1.sol";
 
-contract ChannelTest is Test, ChannelFeeStorageV1 {
-// ChannelFactory channelFactory;
-// Channel channelInstance;
-// address channelAddress;
-// address nick = 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C;
-
-// function setUp() public {
-//     ChannelFactory channelFactory = new ChannelFactory();
-//     // Create a new ERC1155 contract
-
-//     FeePair[] memory fees = new FeePair[](2);
-//     fees[0] = FeePair({addr: 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C, amount: 111000000000000});
-//     fees[1] = FeePair({addr: 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C, amount: 111000000000000});
-
-//     bytes memory encodedFixedFees = abi.encode(fees);
-
-//     uint256[] memory dynamicFees = new uint256[](3);
-//     dynamicFees[0] = 333000000000000;
-//     dynamicFees[1] = 111000000000000;
-//     dynamicFees[2] = 111000000000000;
-
-//     bytes memory encodedDynamicFees = abi.encode(dynamicFees);
-
-//     address channelAddress =
-//     channelFactory.createChannel("https://example.com/api/token/", encodedFixedFees, encodedDynamicFees);
-
-//     channelInstance = Channel(channelAddress);
-// }
-
-// function test_createToken() public {
-//     // Create a new token
-//     uint256 tokenId = channelInstance.createToken("https://example.com/api/token/0", nick, 100, nick);
-//     console.log("New ERC1155 token id:", tokenId);
-
-//     // get the token
-//     Channel.TokenConfig memory token = channelInstance.getTokens(tokenId);
-//     console.log("Token uri:", token.uri);
-//     console.log("Token maxSupply:", token.maxSupply);
-//     console.log("Token totalMinted:", token.totalMinted);
-//     console.log("Token author:", token.author);
-//     console.log("Token createReferral:", token.createReferral);
-
-//     console.log("Channel Mint Price With Mint Referral:", channelInstance.getTokenMintPrice(tokenId, true));
-//     console.log("Channel Mint Price Without Mint Referral:", channelInstance.getTokenMintPrice(tokenId, false));
-// }
-
-// function test_mint() public {
-//     // Create a new token
-//     uint256 tokenId = channelInstance.createToken("https://example.com/api/token/0", 100);
-
-//     // Mint some tokens
-//     channelInstance.mint(nick, tokenId, 10, "0x0");
-//     uint256 balance = channelInstance.balanceOf(nick, tokenId);
-//     console.log("Balance of token:", balance);
-// }
+contract ChannelTest is Test {
+    // ChannelFactory channelFactory;
+    // Channel channelInstance;
+    // address channelAddress;
+    // address nick = 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C;
+    // function setUp() public {
+    //     ChannelFactory channelFactory = new ChannelFactory();
+    //     // Create a new ERC1155 contract
+    //     FeePair[] memory fees = new FeePair[](2);
+    //     fees[0] = FeePair({addr: 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C, amount: 111000000000000});
+    //     fees[1] = FeePair({addr: 0xedcC867bc8B5FEBd0459af17a6f134F41f422f0C, amount: 111000000000000});
+    //     bytes memory encodedFixedFees = abi.encode(fees);
+    //     uint256[] memory dynamicFees = new uint256[](3);
+    //     dynamicFees[0] = 333000000000000;
+    //     dynamicFees[1] = 111000000000000;
+    //     dynamicFees[2] = 111000000000000;
+    //     bytes memory encodedDynamicFees = abi.encode(dynamicFees);
+    //     address channelAddress =
+    //     channelFactory.createChannel("https://example.com/api/token/", encodedFixedFees, encodedDynamicFees);
+    //     channelInstance = Channel(channelAddress);
+    // }
+    // function test_createToken() public {
+    //     // Create a new token
+    //     uint256 tokenId = channelInstance.createToken("https://example.com/api/token/0", nick, 100, nick);
+    //     console.log("New ERC1155 token id:", tokenId);
+    //     // get the token
+    //     Channel.TokenConfig memory token = channelInstance.getTokens(tokenId);
+    //     console.log("Token uri:", token.uri);
+    //     console.log("Token maxSupply:", token.maxSupply);
+    //     console.log("Token totalMinted:", token.totalMinted);
+    //     console.log("Token author:", token.author);
+    //     console.log("Token createReferral:", token.createReferral);
+    //     console.log("Channel Mint Price With Mint Referral:", channelInstance.getTokenMintPrice(tokenId, true));
+    //     console.log("Channel Mint Price Without Mint Referral:", channelInstance.getTokenMintPrice(tokenId, false));
+    // }
+    // function test_mint() public {
+    //     // Create a new token
+    //     uint256 tokenId = channelInstance.createToken("https://example.com/api/token/0", 100);
+    //     // Mint some tokens
+    //     channelInstance.mint(nick, tokenId, 10, "0x0");
+    //     uint256 balance = channelInstance.balanceOf(nick, tokenId);
+    //     console.log("Balance of token:", balance);
+    // }
 }
